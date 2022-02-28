ee4576979bed4775a1ce70802f8d2bf3df4f83ee
juanseferrari <juansegundoferrari@gmail.com>
2022-02-28 03:06:57 -0300

    detalle google

--- public/js/login.js
+++ public/js/login.js
@@ -16,11 +16,6 @@ function onSignIn(googleUser) {
   nombre.innerText = profile.getName()
   mail.innerText = profile.getEmail()
   foto.src = profile.getImageUrl()
-  console.log("ESTAMOS EN OnSignIn")
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
   var myUserEntity = {};
   myUserEntity.Id = profile.getId();
