dbf7253b94d709d20df19bee5dfb0f2fb733648b
juanseferrari <juansegundoferrari@gmail.com>
2022-11-20 21:11:39 +0000

    full page mejoras

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,10 +1,12 @@
 const goToWapp = document.querySelector("#goToWapp")
-const text = document.querySelector("#message")
+const text_contact = document.querySelector("#message")
+const name_contact = document.querySelector("#name")
+const email_contact = document.querySelector("#email")
 
 goToWapp.addEventListener("click", function(e){
     e.preventDefault()
-    console.log(text.value)
-    window.open("https://wa.me/+5491140494130?text=" + text.value, '_blank').focus();
+    var text_to_send = "Nombre: " + name_contact.value + "%0aEmail: "+ email_contact.value + "%0aMensaje: "+ text_contact.value
+    window.open("https://wa.me/+5491140494130?text=" + text_to_send, '_blank').focus();
 })
 
   //window.location.href="https://wa.me/+5491140494130?text=" + text;
