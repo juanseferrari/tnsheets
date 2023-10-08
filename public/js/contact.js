ed8d2cab728933af4a0d8915008fce7f067036a2
juanseferrari <juansegundoferrari@gmail.com>
2023-10-08 20:03:28 +0200

    cambio whatsapp

--- public/js/contact.js
+++ public/js/contact.js
@@ -6,7 +6,7 @@ const email_contact = document.querySelector("#email")
 goToWapp.addEventListener("click", function(e){
     e.preventDefault()
     var text_to_send = "Nombre: " + name_contact.value + "%0aEmail: "+ email_contact.value + "%0aMensaje: "+ text_contact.value
-    window.open("https://wa.me/+5491140494130?text=" + text_to_send, '_blank').focus();
+    window.open("https://wa.me/+34628770275?text=" + text_to_send, '_blank').focus();
 })
 
   //window.location.href="https://wa.me/+5491140494130?text=" + text;
