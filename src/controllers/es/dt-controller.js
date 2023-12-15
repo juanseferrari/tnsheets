1078f7135f4d2386543b8f32221158d2e6316dc2
juanseferrari <juansegundoferrari@gmail.com>
2023-12-15 10:09:46 -0300

    uninstalled date null

--- src/controllers/es/dt-controller.js
+++ src/controllers/es/dt-controller.js
@@ -161,6 +161,7 @@ const dtController = {
         "connection": "drive-to-tiendanube",
         "google_user_id": google_user_id,
         "active": "true",
+        "uninstalled_date": null,
         "user_name": tn_user_data['name']['es'],
         "user_email": tn_user_data['email'],
         "user_logo": user_logo,
--- src/controllers/es/wo-controller.js
+++ src/controllers/es/wo-controller.js
@@ -95,6 +95,7 @@ const woController = {
           "connection": "woocommerce",
           //"google_user_id": google_user_id,
           "active": "true",
+          "uninstalled_date": null,
           //"user_name": sh_shop_data["shop"]['name'],
           //"user_email": sh_shop_data["shop"]['email'],
           //"user_logo": user_logo,
