5504a6a1945290d660632459f12fd742cbb95e90
juanseferrari <juansegundoferrari@gmail.com>
2024-03-05 00:33:30 -0300

    google auth arreglo

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -5,6 +5,9 @@ const apiController = require('../controllers/api-controller');
 const paymentsController = require('../controllers/payments-controller');
 const googleController = require('../controllers/google-controller');
 
+// Middlewares
+const checkAuth = require('../middlewares/check-auth');
+const generalMid = require('../middlewares/general-mid');
 
 /* PUBLIC APIS*/
 //tienen que ser kebab-case
@@ -19,8 +22,8 @@ router.get('/subscription-status', paymentsController.checkSubscription)
 router.post('/redeem-payment', paymentsController.redeemPayment)
 
 /* GOOGLE AUTH DATA */
-router.post('/google-auth',googleController.googleoauth)
-router.post('/google-auth2',googleController.googleoauth2)
+router.post('/google-auth',generalMid,googleController.googleoauth)
+router.post('/google-auth2',generalMid,googleController.googleoauth2)
 
 //router.get('/google-login',googleController.googleLink)
 
